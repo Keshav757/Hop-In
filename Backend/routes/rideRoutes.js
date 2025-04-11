7e04a37c695dda9fc025c8096d4a4cd4e0c76f16
Siddharth Kamlekar <saisiddu02@gmail.com>
2025-04-11 21:47:20 +0530

    Token

--- Backend/routes/rideRoutes.js
+++ Backend/routes/rideRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const rideController = require('../controllers/rideController');
+const jwt=require('jsonwebtoken')
 const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization');
+    const token =req.header('Authorization')?.split(' ')[1];
     if (!token) return res.status(403).send('Access denied');
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;  // User's data is available in req.user
+        console.log("User",decoded)
         next();
     } catch (error) {
         res.status(400).send('Invalid token');
